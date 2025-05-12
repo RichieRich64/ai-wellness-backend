c75e616e703e3b4321f8a2b7df45612a30fa6643
Richard <rico4live@gmail.com>
2025-05-12 10:34:55 +0100

    update2

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,51 @@
 import express from "express";
-// import { Router, Request, Response } from "express";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import session from "express-session";
-// import passport from "passport";
-// import "./config/passport";
-// import authRoutes from "./routes/auth";
-// import calendarRoutes from "./routes/calendar";
+import { Router, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import session from "express-session";
+import passport from "passport";
+import "./config/passport";
+import authRoutes from "./routes/auth";
+import calendarRoutes from "./routes/calendar";
 
-// dotenv.config();
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middlewares
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET!,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       secure: false,
-//       httpOnly: true,
-//       sameSite: "lax", // or "none" if you’re using HTTPS
-//     },
-//   })
-// );
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      sameSite: "lax", // or "none" if you’re using HTTPS
+    },
+  })
+);
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
-// app.get("/health", async (req: Request, res: Response) => {
-//   res.send({ message: "health OK!" });
-// });
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
 
 // Routes
-// app.use("/auth", authRoutes);
-// app.use("/calendar", calendarRoutes);
+app.use("/auth", authRoutes);
+app.use("/calendar", calendarRoutes);
 
 // Google Add-on trigger handler
 app.all("/calendar-add-on/homepage", (req, res) => {
@@ -347,13 +347,13 @@ app.all("/calendar-add-on/handleSkipSetup", (req, res) => {
 //   return;
 // });
 
-// app.get("/me", (req, res) => {
-//   if (!req.user) {
-//     res.status(401).send("Not logged in");
-//     return;
-//   }
-//   res.send(req.user);
-// });
+app.get("/me", (req, res) => {
+  if (!req.user) {
+    res.status(401).send("Not logged in");
+    return;
+  }
+  res.send(req.user);
+});
 
 app.listen(PORT, () =>
   console.log(`Backend running on http://localhost:${PORT}`)
